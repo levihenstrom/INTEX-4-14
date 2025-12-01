af1ddcda5ef8e4f5463e555b9eb50b5b3be10b38
Levihenny <levi.henstrom@gmail.com>
2025-12-01 10:07:01 -0700

    secure all

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -2,3 +2,4 @@
 # Place in .platform/hooks/postdeploy directory
 sudo certbot -n -d ellarises.is404.net --nginx --agree-tos --email lhenstro@byu.edu
 sudo certbot -n -d intex.is404.net --nginx --agree-tos --email lhenstro@byu.edu
+sudo certbot -n -d ellarises414.is404.net --nginx --agree-tos --email lhenstro@byu.edu
